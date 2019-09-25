ddefd53e791ef9f961b7d172f9cf7cdf9051e639
jschepens <jeremy.schepens@orange.fr>
2019-09-25 15:24:04 +0200

    switch 3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var icon = document.getElementsByTagName("i")[0];
-var nav = document.getElementsByTagName("nav")[0];
+var nav = document.getElementsByTagName("ul")[0];
 
 icon.addEventListener("click", function(){
     if (nav.style.display == "none"){
